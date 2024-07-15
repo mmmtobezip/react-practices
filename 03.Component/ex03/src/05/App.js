69c9d5154773d318a864d1202fff842605db316f
mmmtobezip <mmmszip@gmail.com>
2024-07-15 13:11:47 +0900

    feat: useRef with onScroll Event

--- 03.Component/ex03/src/05/App.js
+++ 03.Component/ex03/src/05/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './assets/scss/App.scss';
 
 export default function App() {
+    const outerRef = useRef(null); //mount 전 초기화 
+    const innerRef = useRef(null);
+
     return (
-        <div className={'App'}>
-            <div>
+        <div
+            ref={outerRef} //outer에서 스크롤 생성  -> onScroll Event는 outer에서 받음. 
+            className={'App'}
+            onScroll={e => {
+                //oh: clientHeight 
+                //scrollTop+outerRef.current.clientHeight == innerRef.current.clientHeight) 값이 같아지면, 
+                console.log(outerRef.current.scrollTop, outerRef.current.clientHeight, innerRef.current.clientHeight)
+            }}>
+            <div ref={innerRef}>
                 <ul>
+                    {
+                        Array.from({length:100}, (_, i) => i+1).map(e => <li key={e}>{`아이텀 ${e}입니다.`}</li>)
+                    }
                 </ul>
             </div>
         </div>
--- 03.Component/ex03/src/06/App.js
+++ 03.Component/ex03/src/06/App.js
@@ -6,11 +6,23 @@ export default class App extends Component {
         super(props);
     }
 
+
+    //컴포넌트 자체(객체)에 셋팅하기에 this를 사용해도 괜찮다. 
+    //함수를 만들고 this를 통해 컴포넌트 객체에 셋팅하는 것과 
+    //05으 경우 직접 만들지 않고(함수 실행하고나면 날라기기에) userRef에게 함수 파라미터로 넘어오는 걸 잡아두라는 의미로 closer와 연관된 함수와 함께 사용한다. 
     render() {
         return (
-            <div className={'App'}>
-                <div>
+            <div
+                ref={ref => this.outerRef = ref}
+                className={'App'}
+                onScroll={() => {
+                    console.log(this.outerRef.scrollTop, this.outerRef.clientHeight, this.innerRef.clientHeight)
+                }}>
+                <div ref={ref => this.innerRef = ref}>
                     <ul>
+                    {
+                        Array.from({length:100}, (_, i) => i+1).map(e => <li key={e}>{`아이텀 ${e}입니다.`}</li>)
+                    }
                     </ul>
                 </div>
             </div>
